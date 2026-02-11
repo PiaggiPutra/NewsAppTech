cb9e5e377d0e58477072824ae33b126d7bc7daf5
bintang.putra <bintang.putra@tuntun.co.id>
2026-02-12 00:48:17 +0700

    Setup bottom sheet

---
+++ app/src/main/java/com/piaggi/newsapptech/ui/search/SearchFragment.kt
@@ -0,0 +1,24 @@
+package com.piaggi.newsapptech.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.piaggi.newsapptech.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
